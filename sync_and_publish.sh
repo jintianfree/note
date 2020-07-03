1c07887e5bb81de567d180eef4f8358e1851f0f5
mac@mex <mac@mex>
2020-07-03 14:14:23 +0800

    update

--- sync_and_publish.sh
+++ sync_and_publish.sh
@@ -4,9 +4,9 @@ git commit -m 'update' -a
 git push origin master
 ./hugo
 
-rm -r ../note.github.io/*
-cp -r public/* ../note.github.io/
 cd ../note.github.io/
+rm -r *
+cp -r ../note/public/* .
 git add .
 git commit -m 'update' -a
 git push origin master
