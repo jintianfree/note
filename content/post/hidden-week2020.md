2e9368cee54e73bb2352753b551ebe65f430b340
mac@mex <mac@mex>
2020-07-03 15:25:10 +0800

    update

--- content/post/hidden-week2020.md
+++ content/post/hidden-week2020.md
@@ -1,9 +1,28 @@
 ---
-title: "Week2020"
+title: "note@mex@2020"
 draft: false
 author: "Cheney"
-
 hiddenFromHomePage: true
 ---
 
-# 笔记
+## TODO
+1. go macro   （服务并发模型？  负载均衡机制？ 熔断重试机制？  json rpc怎么转的pb   pdf）
+   go micro  api网关 向后转发  是用的ui/bs阻塞模型  还是nginx 非阻塞模型 web 直接使用了  go自带的反向代理   httputil.NewSingleHostReverseProxy(rp).ServeHTTP(w, r) 反向代理怎么处理的？ 待研究 rpc/ api 使用了什么方式？
+2. k8s
+3. 分布式系统设计
+4. 区块链
+
+
+## 20200703
+TODO:
+- [ ] CPA 数据上传   如果日期格式是日期 无法识别并上传
+- [ ] CPA 数据上传  没有做数据去重
+- [ ] 平台待完善列表 https://kdocs.cn/l/cd2NNdZmO 
+
+
+## 趣旅行
+一些想法
+1. 狗狗考考你    字符游戏
+2. 滑板车道具   滑板跑酷    横向跑跳跃   或者竖向跑 左右躲避障碍
+3. 狗狗跳一跳  参考驴子跳一跳
+4. 狗拉车  需要偷别人的狗
--- sync_and_publish.sh
+++ sync_and_publish.sh
@@ -1,12 +1,36 @@
 
-git add .
-git commit -m 'update' -a
-git push origin master
-./hugo
+sync() {
+  git add .
+  git commit -m 'update' -a
+  git push origin master
+}
 
-cd ../note.github.io/
-rm -r *
-cp -r ../note/public/* .
-git add .
-git commit -m 'update' -a
-git push origin master
+sync_and_publish() {
+  git add .
+  git commit -m 'update' -a
+  git push origin master
+  ./hugo
+  
+  cd ../note.github.io/
+  rm -r *
+  cp -r ../note/public/* .
+  git add .
+  git commit -m 'update' -a
+  git push origin master
+}
+
+loacl_server() {
+    ./hugo server
+}
+
+case $1 in
+"srv")
+   loacl_server 
+;;
+"sync")
+    sync
+;;
+"pub")
+    sync_and_publish
+;;
+esac
